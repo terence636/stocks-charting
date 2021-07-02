baba1958d2440f8f390329e4244dc99f7bf5038b
terencechan <terence.kfc@gmail.com>
2021-07-02 12:59:58 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,80 +1,18 @@
-import React, {useEffect, useState} from 'react'
-import Plot from 'react-plotly.js';
-// import Chart from "react-google-charts";
-import stockData from './stockData.js'
+import React from "react";
+import Main from "./components/Main"
 
-const baseURL = `http://www.alphavantage.co/query?`
-const functionType = 'function=TIME_SERIES_DAILY_ADJUSTED'
-const symbol = '&symbol=BABA'
-const outputSize = '&outputsize=compact'
-const apiKey = '&apikey=XW0YLMZBBXDYRUEK'
-const apiKeyDemo = 'demo'
-const URL = baseURL + functionType + symbol + outputSize + apiKey
+// import dotenv from 'dotenv'
+// dotenv.config()
 
-const stockXValuesFunction = []
-const stockYValuesFunction = []
-// let stockXYValues = []
-// const allPoints = []
-
-
-const App = () => {
-    // Your State is here
-    // const [dataPoints, setDataPoints] = useState([])
-
-    //Fetch data here
-    useEffect(()=> {
-        fetch(URL)
-        .then((res) => {
-            console.log(res);
-              return res.json()
-          })
-          .then((data) => {
-            console.log("data", data);
-            
-            // Collect date and price point
-            for(let key in data['Time Series (Daily)']) {
-
-                // stockXYValues.push(new Date(key))
-                // stockXYValues.push(data['Time Series (Daily)'][key]['4. close'])
-                // allPoints.push(stockXYValues)
-                // stockXYValues = []
-                stockXValuesFunction.push(key)
-                stockYValuesFunction.push(data['Time Series (Daily)'][key]['4. close'])
-
-            }
-            //setState
-            // setDataPoint({stockXValues,stockYValues})
-            // setDataPoints(allPoints)
-            // console.log("allPoints",allPoints)
-          })  
-
-    },[])
-    
-    // console.log(...dataPoints)
-    // if(dataPoints === [])
-    //   return
-
-
-    return (
-        <div>
-            <h1>Stocks</h1>
-   
-            <Plot
-            data={[
-            {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      />
-        </div>
-    )
-    
+function App() {
+  const apiKey = process.env.REACT_APP_VANTAGE_API_KEY
+  console.log("apikey at app",apiKey)
+  return (
+    <div>
+      <Main />
+    </div>
+  );
 }
+
 
 export default App
--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Plot from "react-plotly.js";
+// import Chart from "react-google-charts";
+import stockData from "../stockData.js";
 
-const main = () => {
-    return (
-        <div>
-            <h1>test</h1>
-        </div>
-    )
-}
+const baseURL = `http://www.alphavantage.co/query?`;
+const functionType = "function=TIME_SERIES_DAILY_ADJUSTED";
+const symbol = "&symbol=BABA";
+const outputSize = "&outputsize=compact";
+const apiKey = "&apikey=" + process.env.REACT_APP_VANTAGE_API_KEY;
+const apiKeyDemo = "demo";
+const URL = baseURL + functionType + symbol + outputSize + apiKeyDemo;
 
-export default main
+console.log("apiKey", apiKey);
+const stockXValuesFunction = [];
+const stockYValuesFunction = [];
+// let stockXYValues = []
+// const allPoints = []
+
+const Main = () => {
+  // Your State is here
+  const [dataPoints, setDataPoints] = useState(stockData);
+  console.log("dataPoints", dataPoints);
+  //Fetch data here
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+
+        // Collect date and price point
+        for (let key in data["Time Series (Daily)"]) {
+          // stockXYValues.push(new Date(key))
+          // stockXYValues.push(data['Time Series (Daily)'][key]['4. close'])
+          // allPoints.push(stockXYValues)
+          // stockXYValues = []
+          stockXValuesFunction.push(key);
+          stockYValuesFunction.push(
+            data["Time Series (Daily)"][key]["4. close"]
+          );
+        }
+        //setState
+        // setDataPoint({stockXValues,stockYValues})
+        // setDataPoints(allPoints)
+        // console.log("allPoints",allPoints)
+        // console.log("X",stockXValuesFunction)
+        // console.log("Y",stockYValuesFunction)
+      });
+  }, []);
+
+  // Collect date and price point
+  for (let key in dataPoints["Time Series (Daily)"]) {
+    // stockXYValues.push(new Date(key))
+    // stockXYValues.push(data['Time Series (Daily)'][key]['4. close'])
+    // allPoints.push(stockXYValues)
+    // stockXYValues = []
+    stockXValuesFunction.push(key);
+    stockYValuesFunction.push(
+      dataPoints["Time Series (Daily)"][key]["4. close"]
+    );
+  }
+  console.log("X", stockXValuesFunction);
+  console.log("Y", stockYValuesFunction);
+  // console.log(...dataPoints)
+  // if(dataPoints === [])
+  //   return
+
+  return (
+    <div>
+      <h1>Stocks</h1>
+      
+      <Plot
+        data={[
+          {
+            x: stockXValuesFunction,
+            y: stockYValuesFunction,
+            type: "scatter",
+            mode: "lines",
+            name: "Lines",
+            line: {
+                dash: 'solid',
+                width: 4
+              },
+            marker: { color: "blue" },
+          },
+        ]}
+        layout={{ width:720, height: 440, title: dataPoints["Meta Data"]["2. Symbol"] }}
+      />
+
+      <p>X-Value: {stockXValuesFunction}</p>
+      <p>Y-Value: {stockYValuesFunction}</p>
+    </div>
+  );
+};
+
+export default Main;
